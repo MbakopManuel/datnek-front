b45a597d7a6a14ff8e4a0c0df8b7145f9452d871
Manuel <manuel.b@intekor.ca>
2020-11-05 16:30:23 +0100

    localization

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+const API = "http://localhost:5000/api/"
 
 export const environment = {
   production: false
@@ -14,3 +15,7 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+export const Link = {
+  _langue: `${API}langue`
+}
